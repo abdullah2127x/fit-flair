68d20415bc850a114cf51613bd9a3f152cb6e4e2
Abdullah Qureshi <mabdullahqureshi583@gmail.com>
2025-08-19 19:14:16 +0500

    make the hero slide comp dynamic

--- src/components/custom/Hero.tsx
+++ src/components/custom/Hero.tsx
@@ -6,18 +6,40 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import PrimaryHeading from "./PrimaryHeading";
 import SecondaryHeading from "./SecondaryHeading";
+import { Autoplay, Mousewheel } from "swiper/modules";
 
-const HeroSlideOne = () => {
+type HeroSlideProps = {
+  title: string;
+  subTitle: string;
+  buttons: {
+    label: string;
+    href: string;
+  }[];
+  imageSide: "left" | "right";
+  imageUrl: string;
+};
+const HeroSlide = ({
+  title,
+  subTitle,
+  buttons,
+  imageSide,
+  imageUrl,
+}: HeroSlideProps) => {
   return (
-    <section className="relative w-full h-[60vh] md:h-[70vh] flex justify-center md:justify-end items-end md:items-center md:px-[14vw] py-[10%] md:py-0    ">
-
+    <section
+      className={`relative w-full h-[60vh] md:h-[70vh] flex justify-center ${
+        imageSide == "left" ? "md:justify-end" : "justify-start"
+      } items-end md:items-center md:px-[14vw] py-[10%] md:py-0    `}
+    >
       {/* Background Image */}
       <Image
-        src="/images/hero/heroSlideOneImage.webp"
+        src={imageUrl}
         alt="Adventure"
         fill
         priority
-        className="object-cover object-[19%_30%] -z-10"
+        className={`object-cover md:object-[19%_30%]  -z-10 
+          ${imageSide === "left" ? "object-[19%_30%]" : "object-[81%_70%]"}
+          `}
       />
 
       {/* Overlay (optional dark layer for better text readability) */}
@@ -25,23 +47,21 @@ const HeroSlideOne = () => {
 
       {/* Content */}
       <div className="flex gap-4 flex-col items-center ">
-        <PrimaryHeading className="text-secondary md:text-primary">MAN</PrimaryHeading>
-        <SecondaryHeading className="text-secondary md:text-primary" >INTERMIX &apos;25</SecondaryHeading>
+        <PrimaryHeading className="text-secondary md:text-primary">
+          {title}
+        </PrimaryHeading>
+        <SecondaryHeading className="text-secondary md:text-primary">
+          {subTitle}
+        </SecondaryHeading>
 
         <div className="flex gap-x-4">
-          {[
-            {
-              label: "Stiched",
-              href: "/get-started",
-            },
-            {
-              label: "UNSTICHED",
-              href: "/get-started",
-            },
-          ].map((item) => (
-            <Button className="bg-secondary text-primary md:bg-primary md:text-secondary" key={item.label} size={"lg"}>
-
-              {item.label}
+          {buttons.map((button) => (
+            <Button
+              className="bg-secondary text-primary md:bg-primary md:text-secondary"
+              key={button.label}
+              size={"lg"}
+            >
+              {button.label}
             </Button>
           ))}
         </div>
@@ -50,13 +70,44 @@ const HeroSlideOne = () => {
   );
 };
 
-
 export default function Hero() {
+  const slideOne: HeroSlideProps = {
+    title: "MAN ",
+    subTitle: "INTERMIX '25",
+    buttons: [
+      {
+        label: "Stiched",
+        href: "/get-started",
+      },
+      {
+        label: "UNSTICHED",
+        href: "/get-started",
+      },
+    ],
+    imageSide: "left",
+    imageUrl: "/images/hero/heroSlideOneImage.webp",
+  };
   return (
     <section className="w-full md:mt-14">
-      <Swiper slidesPerView={1} loop={true}>
+      <Swiper
+        modules={[Autoplay, Mousewheel]}
+        spaceBetween={30}
+        slidesPerView={1}
+        loop={true}
+        allowTouchMove={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+      >
         <SwiperSlide>
-          <HeroSlideOne />
+          <HeroSlide
+            title={slideOne.title}
+            subTitle={slideOne.subTitle}
+            buttons={slideOne.buttons}
+            imageSide={slideOne.imageSide}
+            imageUrl={slideOne.imageUrl}
+          />
         </SwiperSlide>
       </Swiper>
     </section>
