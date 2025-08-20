c8e5d0b395e7421e44776d7dea65b908c4a2fadc
Abdullah Qureshi <mabdullahqureshi583@gmail.com>
2025-08-20 12:32:18 +0500

    stable version now adding sanity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/custom/Hero";
+import NewIn from "@/components/custom/NewIn";
 
 export default function Home() {
   return (
     <div className="md:mt-12">
       <Hero />
+      <NewIn />
      
     </div>
   );
---
+++ src/components/custom/NewIn.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import RippleCircle from "./CircleImage";
+
+const NewIn = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h2 className="text-3xl md:text-5xl">WHAT'S NEW</h2>
+      <RippleCircle />
+
+    </div>
+  );
+};
+
+export default NewIn;
